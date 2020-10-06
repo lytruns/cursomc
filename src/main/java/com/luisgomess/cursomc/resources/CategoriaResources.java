eae77a675090d08994624a70690ef7a8ae5d1ec9
Luis Gomes <luis.augusto.gomes.rocha@gmail.com>
2020-10-06 00:32:50 -0300

    TEstando a PRimeira Classe de Domínio

--- src/main/java/com/luisgomess/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/luisgomess/cursomc/resources/CategoriaResources.java
@@ -1,16 +1,29 @@
 package com.luisgomess.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luisgomess.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public	String lisar() {
-		return "Rest está funcionando!";
+	public	List<Categoria> lisar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+				
+		return lista;
 	}
 
 }
