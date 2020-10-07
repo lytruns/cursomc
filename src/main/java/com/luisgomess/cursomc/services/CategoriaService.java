54bb4b5d12ba9ac179fdfa16e7a3b9b1746d4414
Luis Gomes <luis.augusto.gomes.rocha@gmail.com>
2020-10-07 00:34:23 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/luisgomess/cursomc/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.luisgomess.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luisgomess.cursomc.domain.Categoria;
+import com.luisgomess.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null); 
+	
+	
+	
+	}
+
+}
